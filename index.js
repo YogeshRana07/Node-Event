f08b150d1e51c4b410c6c6000ec99f885579c24d
Yogesh Rana <51941692+YogeshRana07@users.noreply.github.com>
2020-06-24 23:59:50 +0530

    upload files

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ const tempCard=fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-
 const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj=JSON.parse(data);
 
-const slugs=dataObj.map(el=>slugify(el.sportName,{lower:true}));
-console.log(slugs);
+// const slugs=dataObj.map(el=>slugify(el.description,{lower:true}));
+// console.log(slugs);
 
 const server=http.createServer((req,res)=>{
 	console.log(req.url);
 	const {query,pathname}=(url.parse(req.url,true));
 	 // pathName=req.url;
+	 // console.log(query);
+
 
 	if(pathname==='/' || pathname==='/overview'){
         res.writeHead(200,{'Content-type':'text/html'});
@@ -29,6 +31,8 @@ const server=http.createServer((req,res)=>{
         const cardsHtml=dataObj.map(el=>replaceTemplate(tempCard,el)).join('');
 
         const output=tempOverview.replace('{%SPORT_CARDS%}',cardsHtml);
+        // console.log(cardsHtml);
+
 
 		res.end(output);
 	}
--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -2,7 +2,8 @@ module.exports =(temp,sport)=>{
 	// console.log(`${sport.id}`);
 
 	let output=temp.replace(/{%SPORTNAME%}/g,sport.sportName);
-	  output=output.replace(/{%IMAGE%}/g,sport.image);
+	 output=output.replace(/{%IMAGE%}/g,sport.image);
+	 output=output.replace(/{%EMOJI%}/g,sport.emoji);
 	 output=output.replace(/{%DESCRIPTION%}/g,sport.description);
 	 output=output.replace(/{%ID%}/g,sport.id);
 
